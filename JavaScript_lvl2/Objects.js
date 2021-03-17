3599465a486ffc46cfe9ad837e3652edde2c4c65
Slawomir Serafin <margolek555@gmail.com>
2021-03-17 09:01:16 +0100

    Update previus section

--- JavaScript_lvl2/Objects.js
+++ JavaScript_lvl2/Objects.js
@@ -1,1 +1,21 @@
-console.log("Some text here")
+var canInfo = {make:'Toyota',year:1990,model:"Camry"}
+var myNewO = {a:'Hello',b:[1,2,3],c:{inside:['a','b']}}
+
+for (key in canInfo){
+	console.log(key)
+	console.log(canInfo[key])
+}
+
+var simple = {
+	prop: "Hello",
+	myMethod: function(){
+		console.log("My methos was called")
+	}
+}
+
+var myObj = {
+	name : "Slawek",
+	greet : function(){
+		console.log("Hello "+this.name)
+	}
+}
